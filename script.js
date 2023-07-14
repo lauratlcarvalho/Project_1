fe714c0e3ad17c00c26652e5fed19ad491ef73a6
lauratlcarvalho <lauratlcarvalho@gmail.com>
2023-07-14 02:03:08 +0100

    Added new photos to files and made changes in contact.html, index.html, parks.html, script.js and style.css.

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+/* ------------------ HEADER PARKS AND CONTACT PAGE --------------------- */
+
 /* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
 function myFunction() {
     var x = document.getElementById("myLinks");
@@ -8,6 +10,40 @@ function myFunction() {
     }
   }
 
+/* ------------------ MAIN PAGE ------------------------------------------- */
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("demo");
+  let captionText = document.getElementById("caption");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+  captionText.innerHTML = dots[slideIndex-1].alt;
+}
+
+/* ------------------ CONTACT PAGE ----------------------------------------- */
 
   const message =
   "Thank you for sending us a message! We will respond shortly.";
